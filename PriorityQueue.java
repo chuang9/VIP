f4f47dddb6572ed80b9c27f3b38d7cd4204e2c4e
jasonlam00 <jlam6@stuy.edu>
2017-04-21 11:32:59 -0400

    Update PriorityQueue.java

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,9 +1,11 @@
 public interface PriorityQueue {
 
     public void add(Ticket x);
+    
     public boolean isEmpty;
+    
     public Ticket peekMin;
+    
     public Ticket removeMin;
-    public String toString;
 
 }
