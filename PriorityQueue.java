e1aa385b75fc8ca65903a2a31f10a7443a4c92fe
ilam1 <ilam1@stuy.edu>
2017-04-23 12:41:57 -0400

    Fixed syntax errors

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -2,10 +2,10 @@ public interface PriorityQueue {
 
     public void add(Ticket x);
     
-    public boolean isEmpty;
+    public boolean isEmpty();
     
-    public Ticket peekMin;
+    public Ticket peekMin();
     
-    public Ticket removeMin;
+    public Ticket removeMin();
 
 }
