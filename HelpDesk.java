4256b5e4cfcc6ff6a017f7adb904ce87143f25ae
ilam1 <ilam1@stuy.edu>
2017-04-23 14:09:24 -0400

    Fixed syntax errors, added main

--- HelpDesk.java
+++ HelpDesk.java
@@ -5,49 +5,45 @@
 //2017-04-24
 
 import cs1.Keyboard;
-import java.util.*;
-public class HelpDesk {
 
+import java.util.*;
+
+public class HelpDesk {
     //Instance Variables
     private ArrayPriorityQueue _data; //Queue of all tickets that have yet to be processed
-    private ArrayList _ID; //List of all ID's (to ensure a unique ID)
-
+    private ArrayList<Integer> _ID; //List of all ID's (to ensure a unique ID)
+    
     //Constructor
-    public HelpDesk() {
-	_data = new ArrayPriorityQueue();
-	_ID = new ArrayList();
-    }
-
+	public HelpDesk() {
+	    _data = new ArrayPriorityQueue();
+	    _ID = new ArrayList<>();
+	}
+    
     public void addTicket() {
 	//ID of user
-	int newID = (int)(Math.random() * 100000000);
+	int newID = (int) (Math.random() * 100000000);
 	while (_ID.contains(newID))
-	    newID = (int)(Math.random() * 100000000);
-		
+	    newID = (int) (Math.random() * 100000000);
+	
 	//Name of user
-	System.out.println("Welcome! What is your name?");
+	System.out.print("Please enter your name: ");
 	String name = Keyboard.readString();
-
+	
 	//VIP status of user
 	System.out.println("\nWhat are you?\n\t0. Boss\n\t1. Manager\n\t2. Staff\n\t3. Member\n\t4. Family Member/ Acquaintance\n\t5. Passing Stranger");
 	int priority = Keyboard.readInt();
-
+	
 	//Problem of user
 	System.out.println("\nPlease describe your problem.");
 	String problem = Keyboard.readString();
-
+	
 	//Adding the ticket
 	Ticket toAdd = new Ticket(newID, priority, name, problem);
-
+	_data.add(toAdd);
+	
 	//End message to keep up relations
 	System.out.println("Your ticket has successfully been added to the queue! Please check back later for your solution");
     }
-
-    /*	//Viewing all tickets (not that the user can choose which ticket to solve, just to add helpful stress)
-    public void viewTicket() {
-	for (int i = 0; i < _data.size(); i++)
-	    System.out.println(_data.get(0));
-	    }*/
 
     //Solving a ticket
     public void solveTicket() {
@@ -59,20 +55,31 @@ public class HelpDesk {
 	    Ticket workingTic = _data.peekMin();
 	    
 	    //Prints the problem of the ticket for the user for ease of reference
-	    System.out.println(workingTic.getProblem());
+	    System.out.println(workingTic.getName() + " has the following problem: " + workingTic.getProblem());
 	    
 	    //Retrieving solution
 	    System.out.println("Please describe the solution to the aforementioned problem");
 	    String solution = Keyboard.readString();
 	    workingTic.setSolution(solution);
-
+	    
 	    //Removing ticket from queue
 	    _data.removeMin();
-
+	    
 	    //Keeping up relations with the employee
 	    System.out.println("Your solution has been added. Thank you for your hard work!");
-	    
-	}
+		}
     }
- 	
+    
+    //Tests the above method
+    public static void main(String[] args) {
+	HelpDesk hd = new HelpDesk();
+	hd.addTicket();
+	hd.addTicket();
+	System.out.println();
+
+	//Prints all tickets for ease of viewing
+	System.out.println(hd._data);
+	
+	hd.solveTicket();
+    }
 }
